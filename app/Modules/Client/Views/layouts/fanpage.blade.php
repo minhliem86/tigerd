6133247e00092da5c935e1747cf5fb16a0058d7b
Liem Phan <minhliemphp@gmail.com>
2018-02-06 13:11:22 +0700

    update

--- app/Modules/Client/Views/layouts/fanpage.blade.php
+++ app/Modules/Client/Views/layouts/fanpage.blade.php
@@ -1,5 +1,5 @@
 <!--VIDEO FANPAGE-->
-<section class="video-container page-section">
+<section class="video-container page-section" data-aos="fade-up">
     <div class="container">
         <div class="row">
             <div class="col">
@@ -15,7 +15,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm">
-                <div class="fanpage-section">
+                <div class="fanpage-section" data-aos="fade-up-right">
                     <h2 class="title-section mx-auto">Tiger-D FanPage</h2>
                     <div class="fanpage-wrapper">
                         <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTigerD.vn%2F&tabs=timeline&width=500&height=600&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=318148815372649"  height="600" style="border:none;overflow:hidden; width:100%; max-width:500px; margin:0 auto" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
@@ -23,7 +23,7 @@
                 </div>
             </div>
             <div class="col-sm">
-                <div class="video-section">
+                <div class="video-section" data-aos="fade-up-left">
                     <h2 class="title-section mx-auto">Tiger-D Instagram</h2>
                     <div class="instagram-wrapper">
                         <!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/b8b1dde8db8051f2a24ebc1dbf19d2fc.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
