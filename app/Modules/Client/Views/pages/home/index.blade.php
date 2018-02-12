fcaba3f16f8ca4151b9810182b26239e78e68426
Liem Phan <minhliemphp@gmail.com>
2018-02-12 10:59:00 +0700

    up

--- app/Modules/Client/Views/pages/home/index.blade.php
+++ app/Modules/Client/Views/pages/home/index.blade.php
@@ -82,54 +82,70 @@
     <!--END PRODUCT-->
     @endif
 
-    @if(!$news->isEmpty())
-    <!--NEWS-->
-    <section class="news-container page-section">
+    @include('Client::layouts.testimonial')
+
+    <!--VIDEO FANPAGE-->
+    <section class="video-container page-section" data-aos="fade-up">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div class="news-inner" data-aos="fade-up">
-                        <h2 class="title-section mx-auto">Tiger-d News</h2>
-                        <div class="news-body">
-                            <div class="swiper-container" id="swiper-news">
-                                <div class="swiper-wrapper">
-                                    @foreach($news->chunk(2) as $item_news_chunk)
-                                    <div class="swiper-slide">
-                                        <div class="each-news">
-                                            @foreach($item_news_chunk as $item_news)
-                                            <div class="media">
-                                                <a href="{!! route('client.news.detail', $item_news->slug) !!}"><img src="{!! asset($item_news->img_url) !!}" class="mr-5" alt="{!! $item_news->name !!}"></a>
-                                                <div class="media-body">
-                                                    <h3 class="news-name"><a href="{!! route('client.news.detail', $item_news->slug) !!}">{!! $item_news->name !!}</a></h3>
-                                                    <p>{!! $item_news->description !!}</p>
-                                                    <a href="{!! route('client.news.detail', $item_news->slug) !!}" class="readmore float-right">Read more ...</a>
+                    <h2 class="title-section mx-auto">Tiger-D Video</h2>
+                    <div class="video-inner">
+                        <div data-type="youtube" data-video-id="E2WP4VGlDh4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    @if(!$news->isEmpty())
+        <!--NEWS-->
+        <section class="news-container page-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="news-inner" data-aos="fade-up">
+                            <h2 class="title-section mx-auto">Tiger-d News</h2>
+                            <div class="news-body">
+                                <div class="swiper-container" id="swiper-news">
+                                    <div class="swiper-wrapper">
+                                        @foreach($news->chunk(2) as $item_news_chunk)
+                                            <div class="swiper-slide">
+                                                <div class="each-news">
+                                                    @foreach($item_news_chunk as $item_news)
+                                                        <div class="media">
+                                                            <a href="{!! route('client.news.detail', $item_news->slug) !!}"><img src="{!! asset($item_news->img_url) !!}" class="mr-5" alt="{!! $item_news->name !!}"></a>
+                                                            <div class="media-body">
+                                                                <h3 class="news-name"><a href="{!! route('client.news.detail', $item_news->slug) !!}">{!! $item_news->name !!}</a></h3>
+                                                                <p>{!! $item_news->description !!}</p>
+                                                                <a href="{!! route('client.news.detail', $item_news->slug) !!}" class="readmore float-right">Read more ...</a>
+                                                            </div>
+                                                        </div>
+                                                    @endforeach
                                                 </div>
                                             </div>
-                                            @endforeach
-                                        </div>
+                                        @endforeach
                                     </div>
-                                    @endforeach
-                                </div>
 
-                                <!-- If we need navigation buttons -->
-                                <div class="swiper-button-prev swiper-button">
+                                    <!-- If we need navigation buttons -->
+                                    <div class="swiper-button-prev swiper-button">
                                     <span class="btn-news-next">
                                         <i class="fa fa-chevron-left"></i>
                                     </span>
-                                </div>
-                                <div class="swiper-button-next swiper-button">
+                                    </div>
+                                    <div class="swiper-button-next swiper-button">
                                     <span class="btn-news-next">
                                         <i class="fa fa-chevron-right"></i>
                                     </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!--END NEWS-->
+        </section>
+        <!--END NEWS-->
     @endif
 
     @include("Client::layouts.fanpage")
